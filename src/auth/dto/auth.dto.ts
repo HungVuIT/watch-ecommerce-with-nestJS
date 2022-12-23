0550474922e62a874097cac3f004708e1d119301
Hung <rox14122000@gmail.com>
2022-12-23 19:08:54 +0700

    fix bug

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -5,7 +5,7 @@ export class authDto {
   @MinLength(4)
   @MaxLength(20)
   @Matches(/^[a-zA-Z0-9_.-]+$/, {
-    message: "username only contain a-z, A-Z, 0-9, '.', '-' and '_'",
+    message: "tài khoản chỉ bao gồm các kí tự chữ thường, chữ hoa, số, gạch ngang, gạch chân và dấu chấm",
   })
   username: string;
 
@@ -14,7 +14,7 @@ export class authDto {
   @MaxLength(20)
   // At least 1 upper case 1 lower case and 1 number, no white space
   @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])^[^ ]+$/, {
-    message: 'password at least 1 upper case 1 lower case and 1 number',
+    message: 'mật khẩu cần tối thiểu 1 chữ thường, 1 chữ hoa, 1 chữ số',
   })
   password: string;
 
